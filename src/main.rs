1c751b50501b90aaf5046fef7eb5f10ad724be86
Porfirio Rodriguez <porfiriorodt@gmail.com>
2021-06-05 23:47:16 -0400

    Todo hecho, funcionando y programa terminado

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,5 @@
-fn main() {
-    println!("Hello, world!");
-}
-
 extern crate color_convert;
 
-#[cfg(test)]
-mod tests {
-
-    use color_convert::handles::map;
-    
-    #[test]
-    fn test_map_name(){
-
-        let color = "White";
-        let toColor = map::map_color_name(&color);
-        assert_eq!(toColor, "#FFFFFF")
-
-    }
-
-    #[test]
-    fn test_map_name_upper(){
-
-        let color = "white";
-        let to_color = map::map_name_to_name(&color);
-        assert_eq!(to_color, "White")
-
-    }
-
-    #[test]
-    fn test_map_name_hex(){
-
-        let color = "B";
-        let toColor = map::map_hex(&color);
-        assert_eq!(toColor, 11)
-
-    }
-
-
+fn main() {
+    println!("Hello, world!");
 }
