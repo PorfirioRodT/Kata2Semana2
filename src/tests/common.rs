424c7141cb07b8987fce35cf8c4bba2df50a9c5b
Porfirio Rodriguez <porfiriorodt@gmail.com>
2021-06-05 22:07:24 -0400

    creacion de la funcion para inicializar los colores

--- src/tests/common.rs
+++ src/tests/common.rs
@@ -1,4 +1,25 @@
 extern crate color_convert;
 use color_convert::color::Color;
 
-pub fn init_color(){}
+pub fn init_color(color:Vec<&str>) -> Vec<Vec<Color>>{
+
+    let mut test_color = vec![];
+
+    for i in 0..Color.len(){
+
+        let result = vec![];
+        result.push(Color::init(color[i], false, false, false));
+        result.push(Color::init(color[i], true, true, true));
+        result.push(Color::init(color[i], true, false, false));
+        result.push(Color::init(color[i], false, true, false));
+        result.push(Color::init(color[i], false, false, true));
+        result.push(Color::init(color[i], true, true, false));
+        result.push(Color::init(color[i], true, false, true));
+        result.push(Color::init(color[i], false, true, true));
+        
+        test_color.push(result);
+
+    }
+    test_color
+
+}
